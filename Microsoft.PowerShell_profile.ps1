aa1c68c79ee6eff1b68a0916a4ce12f02da39836
andrew.monteith <andrew.monteith@kiandra.com.au>
2017-09-01 18:30:46 +1000

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-7d93c81\src\posh-git.psd1'
+
+function gst{
+    git status
+}
+
+function gaa{
+    git add -A
+}
+
+function gcmsg{
+    git commit -m $args
+}
+
+function gco{
+    git checkout $args
+}
+
+function pushup{
+    git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)
+}
+
+cd /
