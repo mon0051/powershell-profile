561a882c0e25e58971c1e8cd3b52588d4d95b68a
andrew.monteith <andrew.monteith@kiandra.com.au>
2018-02-06 12:03:32 +1100

    fix syntax error

--- SetupPowershell.ps1
+++ SetupPowershell.ps1
@@ -1,10 +1,10 @@
 . "$env:USERPROFILE\Documents\WindowsPowershell\Variables.ps1"
 
-if (!Test-Path($env:POSHGIT_SOURCE_CONTAINER_DIR)){
+if (-Not (Test-Path($env:POSHGIT_SOURCE_CONTAINER_DIR))){
     mkdir $env:POSHGIT_SOURCE_CONTAINER_DIR
 }
 
-if (!Test-Path($env:POSHGIT_REPO)){
+if (-Not (Test-Path($env:POSHGIT_REPO))){
     Push-Location $env:POSHGIT_SOURCE_CONTAINER_DIR
     git clone https://github.com/dahlbyk/posh-git.git $env:POSHGIT_REPO_FOLDER_NAME
     Pop-Location
