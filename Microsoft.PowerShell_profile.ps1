9c326567b6a22af5bb2c9c1964df428fb8e1ccb8
andrew.monteith <andrew.monteith@kiandra.com.au>
2018-02-06 11:35:38 +1100

    refactor projects out of scripts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,32 +6,7 @@ Set-PSReadlineOption -BellStyle None
 . "$env:MY_POWERSHELL_HOME\GitShortcuts.ps1"
 . "$env:MY_POWERSHELL_HOME\KenticoScripts.ps1"
 . "$env:MY_POWERSHELL_HOME\NvmFunctions.ps1"
-
-function hc{
-    cd $env:GIT_PROJECTS"\health-canada-reporting-gen2"
-}
-
-function hcf{
-    cd $env:GIT_PROJECTS"\health-canada-reporting-gen2\src\RBH.HealthCanadaGen2.FrontEnd"
-}
-
-function ia{
-    cd $env:GIT_PROJECTS"\investormax"
-}
-
-function kw{
-    cd $env:GIT_PROJECTS"\kiandra-website-2018"
-}
-
-function dionic {
-    $PWD = pwd
-    docker run -ti --rm --publish 8100:8100 -p 35729:35729 --privileged --volume 'c:/Git/projects/gym-app:/myApp:rw' "pam79/ionic-3-framework" ionic $args
-}
-
-function boomdocks {
-    $RUNNING_CONTAINERS = docker ps -q
-    docker kill $RUNNING_CONTAINERS
-}
+. "$env:MY_POWERSHELL_HOME\ProjectShortcuts.ps1"
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -39,11 +14,6 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-function kwmerge{
-    kentico-merge "C:\Git\projects\kiandra-website-2018\Kentico\CMS"
-}
-
-function kmom{
-    gmom
-    kwmerge
+function powerhome{
+    cd $env:MY_POWERSHELL_HOME
 }
