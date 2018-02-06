82d368dfbd13467bccc856f75e8b70e0058525ec
andrew.monteith <andrew.monteith@kiandra.com.au>
2018-02-06 12:00:19 +1100

    add setup script

--- Variables.ps1
+++ Variables.ps1
@@ -1,3 +1,7 @@
 $env:GITHUB_HOST = "git.kiandra.com.au"
 $env:GIT_PROJECTS = "C:\Git\projects"
 $env:MY_POWERSHELL_HOME = "$env:USERPROFILE\Documents\WindowsPowershell"
+$env:POSHGIT_SOURCE_CONTAINER_DIR = "C:\tools\poshgit"
+$env:POSHGIT_REPO_FOLDER_NAME = "dahlbyk-posh-git-7d93c81"
+$env:POSHGIT_REPO = "$env:POSHGIT_SOURCE_CONTAINER_DIR\$env:POSHGIT_REPO_FOLDER_NAME"
+$env:POSHGIT_MODULE_FILE = "$env:POSHGIT_REPO\src\posh-git.psd1"
