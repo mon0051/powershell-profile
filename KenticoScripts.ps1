91141d74aafe2208e8f0b96baeaddec55bbb2ad1
andrew.monteith <andrew.monteith@kiandra.com.au>
2018-04-23 13:20:10 +1000

    refactor for understandability

--- KenticoScripts.ps1
+++ KenticoScripts.ps1
@@ -1,32 +1,46 @@
-function kentico-merge($Path){
-    $bringApplicationOnline = $false
+function AppIsOffline($Path){
+    return Test-Path "$Path\App_Offline.htm"
+}
 
-    # Stops any external Kentico services registered for the instance
-    $services = @(Get-WmiObject win32_service `
+function TakeApplicationOfflineIfRequired($Path){
+    if (!(AppIsOffline))
+    {
+        "<html><head></head><body>Continuous integration restore in progress...</body></html>" > "$Path\App_Offline.htm"
+        return $true
+    }
+    return $false
+}
+
+function RunKenticoContinuousIntegrationTool($Path){
+    & "$Path\bin\ContinuousIntegration.exe" -r -s
+}
+
+function StopExternalKenticoServices($Path){
+     $servicesStopped =  @(Get-WmiObject win32_service `
         | ?{ $_.Name -Like "KenticoCMS*" -And $_.State -Eq "Running" -And $_.PathName -like '*'+ $Path +'"' } `
         | % {
             Stop-Service $_.Name
             return $_.Name
         })
+    return $servicesStopped
+}
 
-    # Creates an 'App_Offline.htm' file to stop the website
-    if (!(Test-Path "$Path\App_Offline.htm"))
-    {
-        "<html><head></head><body>Continuous integration restore in progress...</body></html>" > "$Path\App_Offline.htm"
-        $bringApplicationOnline = $true
-    }
-
-    # Runs the continuous integration restore utility
-    & "$Path\bin\ContinuousIntegration.exe" -r -s
-
-    # Removes the 'App_Offline.htm' file to bring the site back online
-    if ($bringApplicationOnline)
+function BringApplicationOnlineIfRequired($Path, $thisScriptTookApplicationOffline){
+    if ($thisScriptTookApplicationOffline)
     {
         Remove-Item "$Path\App_Offline.htm"
     }
+}
 
-    # Restarts the instance's external services
-    $services | % { Start-Service $_ }
+function StartKenticoServices($ServicesStoped) {
+    $ServicesStoped | % { Start-Service $_ }
+}
 
-    Clear-Variable services, bringApplicationOnline
+function Merge-KenticoCI($Path){
+    $servicesStopped = StopExternalKenticoServices $Path
+    $thisScriptTookApplicationOffline = TakeApplicationOfflineIfRequired $Path
+    RunKenticoContinuousIntegrationTool $Path
+    BringApplicationOnlineIfRequired $Path $thisScriptTookApplicationOffline
+    StartKenticoServices $servicesStopped
+    Clear-Variable servicesStoped, thisScriptTookApplicationOffline
 }
